1c7b633d0663e19531f037fdf6c49ae9e1896f99
Huyvictory <vominhhuy008@gmail.com>
2024-01-05 17:29:19 +0700

    - Support PCKE protection to  avoid code injection attack. - Allow redirect to auth page after logged out. - Return user credentials in identity token provided by IDP.

---
+++ ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Client.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [Authorize]
+        public async Task Logout()
+        {
+            // Clears the local cookie
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Sign out remove credentials of IDP
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
--- ImageGallery.Client/Program.cs
+++ ImageGallery.Client/Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews()
-    .AddJsonOptions(configure => 
+    .AddJsonOptions(configure =>
         configure.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 // create an HttpClient used for accessing the API
@@ -13,6 +15,25 @@ builder.Services.AddHttpClient("APIClient", client =>
     client.BaseAddress = new Uri(builder.Configuration["ImageGalleryAPIRoot"]);
     client.DefaultRequestHeaders.Clear();
     client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+}).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.Authority = "https://localhost:5001";
+    options.ClientId = "imagegalleryclient";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    //options.CallbackPath = new PathString("/signin-oidc");
+    options.SaveTokens = true;
+    options.GetClaimsFromUserInfoEndpoint = true;
 });
 
 var app = builder.Build();
@@ -29,6 +50,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
